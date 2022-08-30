03afc35a8635b600e779fbf0012219224e022e6a
Isaias Santos <95.isaiassantos@gmail.com>
2022-08-30 15:38:50 -0300

    A11 Teste para adicionar produto ao carrinho

--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -11,4 +11,26 @@ class CarrinhoTest extends TestCase
         $classe = class_exists('\\Code\\Carrinho');
         $this->assertTrue($classe);
     }
+
+    public function testAdicionarProdutosNoCarrinho()
+    {
+        $produtoUm = new Produto();
+        $produtoUm->setNome('Teste 1');
+        $produtoUm->setPreco(19.5);
+        $produtoUm->setSlug('produto-1');
+
+        $produtoDois = new Produto();
+        $produtoDois->setNome('Teste 2');
+        $produtoDois->setPreco(10.5);
+        $produtoDois->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+
+        $carrinho->addProduto($produtoUm);
+        $carrinho->addProduto($produtoDois);
+
+        $this->assertIsArray($carrinho->getProdutos());
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
+        $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
+    }
 }

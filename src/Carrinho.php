70f703fd0e992c96b0ee0290208ceae33336d493
Isaias Santos <95.isaiassantos@gmail.com>
2022-08-30 16:09:26 -0300

    A12 Teste Quantidade e Valor total do carrinho

--- src/Carrinho.php
+++ src/Carrinho.php
@@ -16,4 +16,17 @@ class Carrinho
         return $this->produtos;
     }
 
+    public function getQuantidadeProdutos()
+    {
+        return count($this->produtos);
+    }
+
+    public function getValorTotalProdutos()
+    {
+        return array_reduce($this->produtos, function ($total, $produto){
+            $total += $produto->getPreco();
+            return $total;
+        }, 0);
+    }
+
 }
--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -33,4 +33,41 @@ class CarrinhoTest extends TestCase
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
     }
+
+    public function testSeValoresDoProdutoDoCarrinhoConformePassado()
+    {
+        $produtoUm = new Produto();
+        $produtoUm->setNome('Teste 1');
+        $produtoUm->setPreco(19.5);
+        $produtoUm->setSlug('produto-1');
+
+        $carrinho = new Carrinho();
+
+        $carrinho->addProduto($produtoUm);
+
+        $this->assertEquals('Teste 1', $carrinho->getProdutos()[0]->getNome());
+        $this->assertEquals(19.5, $carrinho->getProdutos()[0]->getPreco());
+        $this->assertEquals('produto-1', $carrinho->getProdutos()[0]->getSlug());
+    }
+
+    public function testQuantidadeDeProdutosEValorTotalDoCarrinho()
+    {
+        $produtoUm = new Produto();
+        $produtoUm->setNome('Teste 1');
+        $produtoUm->setPreco(19.5);
+        $produtoUm->setSlug('produto-1');
+
+        $produtoDois = new Produto();
+        $produtoDois->setNome('Teste 2');
+        $produtoDois->setPreco(10);
+        $produtoDois->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+
+        $carrinho->addProduto($produtoUm);
+        $carrinho->addProduto($produtoDois);
+        
+        $this->assertEquals(2 , $carrinho->getQuantidadeProdutos());
+        $this->assertEquals(29.5 , $carrinho->getValorTotalProdutos());
+    }
 }
